9419c7170fe9db97826187a84d113f2d56c1636a
Stanislav <linkoff1994@yandex.ru>
2020-11-13 04:42:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using LZ4;
+
+namespace sakunaTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) 
+            {
+                PrintUsage();
+                return;
+            } 
+            else ArcFile(args[0]);
+        }
+
+        static void ArcFile(string inputFile)
+        {
+            var tempFolder = Path.GetTempPath();
+            var tempFile = $@"{tempFolder}\lz4.tmp";
+            var input = File.ReadAllBytes(inputFile);
+            using (var reader = new BinaryReader(File.OpenRead(inputFile)))
+            {
+                var fileNameWO = Path.GetFileNameWithoutExtension(inputFile);
+                var filenameSize = 96;
+                var header = reader.ReadInt32();
+                var version = reader.ReadInt16();
+                var encryptionType = reader.ReadInt16(); //00 - decrypted, 02 - LZ4
+                var filesCount = reader.ReadInt32();
+                var uSize = reader.ReadInt32();
+                var savePosF = reader.BaseStream.Position;
+                var dataStartOffset = (filenameSize + 16) * filesCount + 16;
+                var dataLength = input.Length - dataStartOffset;
+
+                reader.BaseStream.Seek(dataStartOffset, SeekOrigin.Begin);
+                var data = reader.ReadBytes(dataLength);
+
+                if (encryptionType == 2)
+                {
+                    Console.WriteLine("Compression type: LZ4");
+                    var uData = DecompressLZ4(data, uSize);
+                    File.WriteAllBytes(tempFile, uData);
+                    var dataFile = File.OpenRead(tempFile);
+                    var dataReader = new BinaryReader(dataFile);
+                    reader.BaseStream.Seek(savePosF, SeekOrigin.Begin);
+
+                    for (int i = 0; i < filesCount; i++)
+                    {
+                        var filename = Encoding.UTF8.GetString(reader.ReadBytes(filenameSize));
+                        filename = Regex.Replace(filename, @"\0", "");
+                        var folderName = Path.GetDirectoryName(filename);
+                        var offset = reader.ReadInt32();
+                        var size = reader.ReadInt32();
+                        var unk1 = reader.ReadInt32();
+                        var unk2 = reader.ReadInt32();
+                        var savePos = reader.BaseStream.Position;
+                        dataReader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                        var file = dataReader.ReadBytes(size);
+                        Directory.CreateDirectory($@"{fileNameWO}\{folderName}");
+                        Console.WriteLine($@"Extracting: {filename}");
+                        File.WriteAllBytes($@"{fileNameWO}\{filename}", file);
+                        reader.BaseStream.Seek(savePos, SeekOrigin.Begin);
+                    }
+                    dataFile.Close();
+                    File.Delete(tempFile);
+                }
+                else if (encryptionType == 0)
+                {
+                    Console.WriteLine("Compression type: no compression");
+                    File.WriteAllBytes(tempFile, data);
+                    var dataFile = File.OpenRead(tempFile);
+                    var dataReader = new BinaryReader(dataFile);
+                    reader.BaseStream.Seek(savePosF, SeekOrigin.Begin);
+
+                    for (int i = 0; i < filesCount; i++)
+                    {
+                        var filename = Encoding.UTF8.GetString(reader.ReadBytes(filenameSize));
+                        filename = Regex.Replace(filename, @"\0", "");
+                        var folderName = Path.GetDirectoryName(filename);
+                        var offset = reader.ReadInt32();
+                        var size = reader.ReadInt32();
+                        var unk1 = reader.ReadInt32();
+                        var unk2 = reader.ReadInt32();
+                        var savePos = reader.BaseStream.Position;
+                        dataReader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                        var file = dataReader.ReadBytes(size);
+                        Directory.CreateDirectory($@"{fileNameWO}\{folderName}");
+                        Console.WriteLine($@"Extracting: {filename}");
+                        File.WriteAllBytes($@"{fileNameWO}\{filename}", file);
+                        reader.BaseStream.Seek(savePos, SeekOrigin.Begin);
+                    }
+                    dataFile.Close();
+                    File.Delete(tempFile);
+                }
+            }
+        }
+        static byte[] DecompressLZ4(byte[] inputArray, int uSize)
+        {
+            var uData = LZ4Codec.Decode(inputArray, 0, inputArray.Length, uSize);
+            return uData;
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Sakuna of Rice and Ruin .arc extraction tool v0.2 (Edelweiss Engine)");
+            Console.WriteLine("Created by LinkOFF");
+            Console.WriteLine("");
+            Console.WriteLine("Usage: sakunaTool.exe <archive.arc> (drag&drop .arc file)");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("sakunaTool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sakunaTool")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ec5fa347-e8fd-4c72-b2bf-da5513c5882c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
