b52313abd2cd28b13bcd6bc2f818a173d5ef160e
Stanislav <linkoff1994@yandex.ru>
2020-11-23 23:29:50 +0300

    Update sakunaTool.cs

--- sakunaTool.cs
+++ sakunaTool.cs
@@ -146,22 +146,13 @@ namespace sakunaTool
 
         static long GetDirectorySize(string p)
         {
-            // 1.
-            // Get array of all file names.
             string[] a = Directory.GetFiles(p, "*.*", SearchOption.AllDirectories);
-
-            // 2.
-            // Calculate total bytes of all files in a loop.
             long b = 0;
             foreach (string name in a)
             {
-                // 3.
-                // Use FileInfo to get length of each file.
                 FileInfo info = new FileInfo(name);
                 b += info.Length;
             }
-            // 4.
-            // Return total size
             return b;
         }
 
