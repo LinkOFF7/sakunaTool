a7dce3e0f06fdebd79f3d6b1f7a47692692277c1
Stanislav <linkoff1994@gmail.com>
2022-07-19 20:03:00 +0300

    missing Utils.cs added

---
+++ Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace sakunaTool
+{
+    class BinaryReaderBE : BinaryReader
+    {
+        public BinaryReaderBE(Stream stream) : base(stream) { }
+
+        public override int ReadInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToInt32(data, 0);
+        }
+
+        public Int16 ReadInt16()
+        {
+            var data = base.ReadBytes(2);
+            Array.Reverse(data);
+            return BitConverter.ToInt16(data, 0);
+        }
+
+        public Int64 ReadInt64()
+        {
+            var data = base.ReadBytes(8);
+            Array.Reverse(data);
+            return BitConverter.ToInt64(data, 0);
+        }
+
+        public UInt32 ReadUInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToUInt32(data, 0);
+        }
+
+    }
+}
